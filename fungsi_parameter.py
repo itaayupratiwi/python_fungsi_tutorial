e458dcc8ebf95ea773ce5f73d336e73098261953
Ita Ayu <itaayupratiwi14@gmail.com>
2022-05-27 15:44:33 +0700

    Pnambahan Parameter

---
+++ fungsi_parameter.py
@@ -0,0 +1,12 @@
+def selamat_datang(nama):
+    print('halo',nama,'apa kabar?')
+
+selamat_datang('agung')
+
+def greeting(nama,waktu):
+    print('hello',nama,'selamat',waktu)
+    print('apakah',nama,'sudah datang',waktu,'ini?')
+
+greeting('anji','siang')
+greeting('rufi', 'pagi')
+greeting('ruda', 'malam')
